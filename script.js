e93e32752f5a70d9185e17854dcb77e792228b89
arzezniczak <arzezniczak@arcadiasoft.pl>
2022-05-25 22:45:03 +0200

    Initial github.io commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function setValues() {
+    document.getElementById("shop-name-editable").innerHTML = document.getElementById("shop-name-input").value;
+    document.getElementById("shop-description-editable").innerHTML = document.getElementById("shop-description-input").value;
+}
+
+function showHideMenu() {
+    // if (document.getElementById("interaction-menu").style.visibility == "") {
+    //     document.getElementById("interaction-menu").style.visibility = "hidden";
+    //     document.getElementById("menu-show-hide-button").innerHTML = "➡";
+    // }
+    // else {
+    //     document.getElementById("interaction-menu").style.visibility = "";
+    //     document.getElementById("menu-show-hide-button").innerHTML = "⬅";
+    // }
+    if (window.getComputedStyle(document.getElementById("interaction-menu")).left == "10px") {
+        document.getElementById("interaction-menu").style.left = "-300px";
+        document.getElementById("menu-show-hide-button").innerHTML = "➡";
+    }
+    else {
+        document.getElementById("interaction-menu").style.left = "10px";
+        document.getElementById("menu-show-hide-button").innerHTML = "⬅";
+    }
+}
+
+function countItems() {
+    return document.getElementsByClassName("item-editable").length;
+}
+
+function addItem() {
+    var itemIndex = countItems() + 1;
+    
+    var itemDiv = document.createElement("div");
+    itemDiv.setAttribute("class", "item-editable");
+    var idValue = "item" + itemIndex.toString() + "-editable";
+    itemDiv.setAttribute("id", idValue);
+
+    var itemHeader = document.createElement("h5");
+    itemHeader.innerHTML = "Przedmiot " + itemIndex.toString();
+    itemDiv.appendChild(itemHeader);
+
+    var itemImg = document.createElement("img");
+    itemImg.setAttribute("src", "img/tel.jpg");
+    itemImg.setAttribute("alt", "telefon");
+    itemDiv.appendChild(itemImg);
+
+    document.getElementById("items-container-editable").appendChild(itemDiv);
+}
